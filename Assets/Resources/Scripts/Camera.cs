dd743c9506bfbc8bf7738d1c130c35b8f6a8c166
Matthew Odle <odle.matthew@gmail.com>
2018-07-03 08:14:52 -0500

    change camera movement to axis input

--- Assets/Resources/Scripts/Camera.cs
+++ Assets/Resources/Scripts/Camera.cs
@@ -4,14 +4,12 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour {
 
-    // Update is called once per frame
+    private float speed = 0.1f;
+
     void Update () {
         Vector3 myVector = new Vector3(transform.position.x, transform.position.y, -3);
-        Vector2 touchPosition = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
-        if (Input.GetMouseButton(0)) {
-            myVector.x += (touchPosition.x < Screen.width / 2 ? -0.1f : 0.1f);
-            myVector.y += (touchPosition.y > Screen.height / 2 ? -0.1f : 0.1f);
-        }
+        myVector.x += Input.GetAxisRaw("Horizontal") * speed;
+        myVector.y += Input.GetAxisRaw("Vertical") * speed;
         transform.position = myVector;
     }
 }
