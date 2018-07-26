313bb8a285ef380ca1284c87e1a9732217d22465
Matthew Odle <odle.matthew@gmail.com>
2018-07-25 19:02:02 -0500

    consume materials to create building

--- Assets/Resources/Scripts/BuildingManager.cs
+++ Assets/Resources/Scripts/BuildingManager.cs
@@ -7,7 +7,8 @@ public class BuildingManager : MonoBehaviour {
 
     public static BuildingManager manager;
 
-    public Dictionary<string, Dictionary<string, int>> buildings;
+    public Dictionary<string, Dictionary<string, int>> productionCost;
+    public Dictionary<string, Dictionary<string, int>> buildCost;
 
     void Awake() {
         // singleton pattern
@@ -21,8 +22,10 @@ public class BuildingManager : MonoBehaviour {
     }
 
     private void GenerateBuildings() {
-        buildings = new Dictionary<string, Dictionary<string, int>>();
-        buildings.Add("sawyer", new Dictionary<string, int>() {{"wood", 1}});
+        productionCost = new Dictionary<string, Dictionary<string, int>>();
+        productionCost.Add("sawyer", new Dictionary<string, int>() {{"wood", 1}});
+        buildCost = new Dictionary<string, Dictionary<string, int>>();
+        buildCost.Add("sawyer", new Dictionary<string, int>() {{"wood", 5}, {"rock", 5}});
     }
 
     public void PlaceBuilding(string targetType) {
@@ -31,12 +34,45 @@ public class BuildingManager : MonoBehaviour {
             print("no prefab found for " + targetType);
         }
         if (Input.GetMouseButtonDown(0)) {
-            Vector3 placementLocation = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 actualPlacement = new Vector3(placementLocation.x, placementLocation.y, 0);
-            GameObject theObject = Instantiate(targetPrefab, actualPlacement, Quaternion.identity) as GameObject;
-            theObject.GetComponent<Building>().SetConsumes(buildings[targetType]);
-            TargetBucket.bucket.targets.Add(theObject);
-            theObject.transform.SetParent(transform);
+            Dictionary<string, int> materialsNeeded = buildCost[targetType];
+            if (!CanConstruct(materialsNeeded)) {
+                print ("not enough materials, need " + GetMaterialsRepr(materialsNeeded));
+                return;
+            }
+            ConsumeMaterials(materialsNeeded);
+            InstantiateBuildingObject(targetType, targetPrefab);
         }
+    }
+
+    bool CanConstruct(Dictionary<string, int> materialsNeeded) {
+        foreach (KeyValuePair<string, int> entry in materialsNeeded) {
+            if (entry.Value > ResourceCounter.counter.counts[entry.Key]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string GetMaterialsRepr(Dictionary<string, int> materialsNeeded) {
+        string repr = "";
+        foreach (KeyValuePair<string, int> entry in materialsNeeded) {
+            repr += entry.Key + ": " + entry.Value + ", ";
+        }
+        return repr;
+    }
+
+    void ConsumeMaterials(Dictionary<string, int> materialsNeeded) {
+        foreach (KeyValuePair<string, int> entry in materialsNeeded) {
+            ResourceCounter.counter.counts[entry.Key] -= entry.Value;
+        }
+    }
+
+    void InstantiateBuildingObject(string targetType, Object targetPrefab) {
+        Vector3 placementLocation = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 actualPlacement = new Vector3(placementLocation.x, placementLocation.y, 0);
+        GameObject theObject = Instantiate(targetPrefab, actualPlacement, Quaternion.identity) as GameObject;
+        theObject.GetComponent<Building>().SetConsumes(productionCost[targetType]);
+        TargetBucket.bucket.targets.Add(theObject);
+        theObject.transform.SetParent(transform);
     }
 }
