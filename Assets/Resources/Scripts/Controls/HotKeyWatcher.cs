27b8d2d578d11d4533b340a35c3001a71d007b29
Matthew Odle <odle.matthew@gmail.com>
2018-09-03 00:34:06 -0500

    add toggleable menus

--- Assets/Resources/Scripts/Controls/HotKeyWatcher.cs
+++ Assets/Resources/Scripts/Controls/HotKeyWatcher.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class HotKeyWatcher : MonoBehaviour {
 
     public Dictionary<string, string> hotKeys = new Dictionary<string, string>();
+    public Dictionary<string, GameObject> toggleables = new Dictionary<string, GameObject>();
+    GameObject techTree;
 
     void Start() {
         SetHotKeys();
+        SetObjects();
     }
 
     void SetHotKeys() {
         hotKeys.Add("i", "techTree");
+    }
+
+    void SetObjects() {
+        techTree = GameObject.Find("TechTree");
+        techTree.SetActive(false);
+        toggleables["techTree"] = techTree;
     }
 
 	void Update() {
@@ -21,8 +30,7 @@ public class HotKeyWatcher : MonoBehaviour {
     void CheckInput() {
         foreach (string key in hotKeys.Keys) {
             if (Input.GetKeyDown(key)) {
-                string target = hotKeys[key];
-                print ("pressed " + target);
+                toggleables[hotKeys[key]].SetActive(!techTree.activeSelf);
                 return;
             }
         }
