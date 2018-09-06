c76523c93cf0a84d326b64472682b5434cdff7c7
Matthew Odle <odle.matthew@gmail.com>
2018-09-05 20:06:08 -0500

    convert Resource acounts to TextMeshPro

--- Assets/Resources/Scripts/Counters/ResourceCounter.cs
+++ Assets/Resources/Scripts/Counters/ResourceCounter.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ResourceCounter : MonoBehaviour {
     public static ResourceCounter counter;
     public Dictionary<string, int> counts;
-    public Dictionary<string, Text> counters;
+    public Dictionary<string, TextMeshProUGUI> counters;
     private int startingCount = 100;
 
     public List<string> resources;
@@ -20,19 +21,19 @@ public class ResourceCounter : MonoBehaviour {
         }
 
         counts = new Dictionary<string, int>();
-        counters = new Dictionary<string, Text>();
+        counters = new Dictionary<string, TextMeshProUGUI>();
         resources = new List<string>() {"wood", "rock", "plank"};
 
         foreach (Transform child in transform) {
             if (resources.Contains(child.gameObject.name)) {
-                counters.Add(child.gameObject.name, child.Find("count").GetComponent<Text>());
+                counters.Add(child.gameObject.name, child.Find("count").GetComponent<TextMeshProUGUI>());
                 counts.Add(child.gameObject.name, startingCount);
             }
         }
     }
 
 	void Update () {
-        foreach (KeyValuePair<string, Text> entry in counters) {
+        foreach (KeyValuePair<string, TextMeshProUGUI> entry in counters) {
             entry.Value.text = counts[entry.Key].ToString();
         }
 	}
