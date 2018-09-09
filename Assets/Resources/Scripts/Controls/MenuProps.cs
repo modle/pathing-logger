7eec0253559cb8cf1c580eae6528f7342808689f
Matthew Odle <odle.matthew@gmail.com>
2018-09-09 14:47:22 -0500

    handle menu looping via child objects

---
+++ Assets/Resources/Scripts/Controls/MenuProps.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public class MenuProps : MonoBehaviour {
+    public string hotKey;
+}
--- Assets/Resources/Scripts/Controls/MenuToggler.cs
+++ Assets/Resources/Scripts/Controls/MenuToggler.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class MenuToggler : MonoBehaviour {
 
-    public Dictionary<string, string> hotKeys = new Dictionary<string, string>();
-    public Dictionary<string, GameObject> toggleables = new Dictionary<string, GameObject>();
-    GameObject techTree;
-    GameObject messageLog;
-
-    // child object structure might make more sense here
-    // each child object could be given a ScriptableObject containing a single property: hotkey
-
-    private List<string> menus = new List<string>() {"TechTree", "MessageLog"};
+    Transform menus;
 
     void Start() {
-        SetHotKeys();
         SetObjects();
-        DisableObjects();
-    }
-
-    void SetHotKeys() {
-        hotKeys.Add("i", "TechTree");
-        hotKeys.Add("l", "MessageLog");
     }
 
     void SetObjects() {
-        foreach (string menu in menus) {
-            toggleables[menu] = GameObject.Find(menu);
-        }
-    }
-
-    void DisableObjects() {
-        foreach (KeyValuePair<string, GameObject> entry in toggleables) {
-            entry.Value.SetActive(false);
+        menus = GameObject.Find("Menus").transform;
+        foreach (Transform t in menus) {
+            t.gameObject.SetActive(false);
         }
     }
 
@@ -42,11 +22,10 @@ public class MenuToggler : MonoBehaviour {
 	}
 
     void CheckInput() {
-        foreach (string key in hotKeys.Keys) {
-            if (Input.GetKeyDown(key)) {
-                GameObject obj = toggleables[hotKeys[key]];
-                obj.SetActive(!obj.activeSelf);
-                return;
+        foreach (Transform t in menus) {
+            if (Input.GetKeyDown(t.gameObject.GetComponent<MenuProps>().hotKey)) {
+                t.gameObject.SetActive(!t.gameObject.activeSelf);
+                break;
             }
         }
     }
